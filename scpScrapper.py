0a0b5c53778c175e0a66c95a55d01aa3abcfd8e9
madrouard <54752823+madrouard@users.noreply.github.com>
2020-02-02 19:12:03 -0500

    Update scpScrapper.py

--- scpScrapper.py
+++ scpScrapper.py
@@ -31,7 +31,7 @@ for i in range(10,100):
             file.add_paragraph('Line ' + str(j) + " had an error")
     file.save('./scpArchive/scp-0' + str(i) + ".docx")
 
-for i in range(4940,5000):
+for i in range(100,5000):
     scp = requests.get("http://www.scp-wiki.net/scp-" + str(i))
     soup = BeautifulSoup(scp.text, 'html.parser')
     main_text = soup.select('p')
